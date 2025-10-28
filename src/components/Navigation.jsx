04f98c059d31a7088402302b64cc22f3285c0231
talankinard <kinardtd@email.sc.edu>
2025-10-28 13:16:25 -0400

    added hamburger menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,29 +1,37 @@
 import {Link} from "react-router-dom";
 import "./../css/Navigation.css";
 import "./../css/global.css";
+import {useState} from "react";
 
 const Navigation = () =>
 {
+    const [menuOpen, setMenuOpen] = useState(true);
+
+    const toggleMenu = () => 
+    {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
        <section id="whole-nav">
             <h1>PixelPort</h1>
-            <div id="toggle-nav">
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className={`hamburger-menu ${menuOpen ? "open" : ""}`} id="hamburger-wrapper" onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
             </div>
-            <div id="small-nav">
-                <Link to="/Cart" class="cart-link">Cart</Link>
-                <Link to="/Login" class="login-links">Login</Link>
+            <div id="small-nav" className={menuOpen ? "active" : "hide-small"}>
+                <Link to="/Cart" className="cart-link" onClick={() => setMenuOpen(false)}>Cart</Link>
+                <Link to="/Login" className="login-links" onClick={() => setMenuOpen(false)}>Login</Link>
             </div>
             <nav id="top-nav">
-                <ul id="nav-items" class="hide-small">
-                    <li><Link to="/Home" className="nav-link">Home</Link></li>
-                    <li><Link to="/Browse" className="nav-link">Browse</Link></li>
-                    <li><Link to="/Deals" className="nav-link">Deals</Link></li>
-                    <li><Link to="/News" className="nav-link">News</Link></li>
-                    <li><Link to="/FAQ" className="nav-link">FAQ</Link></li>
-                    <li><Link to="/Contact" className="nav-link">Contact Us</Link></li>
+                <ul id="nav-items" className={menuOpen ? "active" : "hide-small"}>
+                     <li><Link to="/Home" className="nav-link">Home</Link></li>
+                    <li><Link to="/Browse" className="nav-link" >Browse</Link></li>
+                    <li><Link to="/Deals" className="nav-link" >Deals</Link></li>
+                    <li><Link to="/News" className="nav-link" >News</Link></li>
+                    <li><Link to="/FAQ" className="nav-link" >FAQ</Link></li>
+                    <li><Link to="/Contact" className="nav-link" >Contact Us</Link></li>
                 </ul>
             </nav>
         </section>
